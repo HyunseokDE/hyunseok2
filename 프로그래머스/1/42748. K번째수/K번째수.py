2db8740e4d2c044f6f2031c79f48b8c6fd120257
송현석 <106307085+HyunseokDE@users.noreply.github.com>
2025-02-05 09:57:56 +0900

    Update K번째수.py

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -4,7 +4,5 @@ def solution(array, commands):
     for com in commands:
         s = array[com[0]-1:com[1]]
         s.sort()
-        # print('s:',s, 'len(s):',len(s))
-        # print(s[com[2]-1])
         answer.append(s[com[2]-1])
     return answer
