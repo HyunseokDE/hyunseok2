869388585a9d89a9f3f2300dc485b9dfefa83407
송현석 <106307085+HyunseokDE@users.noreply.github.com>
2025-03-10 09:51:13 +0900

    [level 1] Title: 소수 찾기, Time: 3026.99 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,21 +1,18 @@
 import math
 
 def solution(n):
-    answer = 1
+    answer = 0
 
-    if n == 2:
-        return 1
-    else:
-        if n == 3:
-            return 2
-        else:
-            for a in range(4, n+1):
-                for i in range(2,int(math.sqrt(a))+1):
-                    if (a%i != 0):
-                        answer += 1
-                        
-                
-                print(a)
-                        
+    if n < 2:
+        return 0  
+     
+    for a in range(2, n + 1):
+        sosu = True  
+        for i in range(2, int(math.sqrt(a)) + 1):
+            if a % i == 0:
+                sosu = False  
+                break
+        if sosu:
+            answer += 1  
+
     return answer
-
