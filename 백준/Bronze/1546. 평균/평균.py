add854bdfd8cdf0c584fc02669be5baab047220a
송현석 <106307085+HyunseokDE@users.noreply.github.com>
2025-05-01 21:53:51 +0900

    [Bronze I] Title: 평균, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,8 +1,8 @@
 n = int(input()) 
 scores = list(map(int, input().split())) 
 
-max_score = max(scores)
-new_scores = [(s / max_score) * 100 for s in scores]  
+max = max(scores)
+new_scores = [(s / max) * 100 for s in scores]  
 
 average = sum(new_scores) / n  
 print(average)
